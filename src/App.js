b81dbf73616e1f53512037febe112b401013596d
VaibhavDhanani <vaibhavdhanani2711@gmail.com>
2024-05-06 20:51:49 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 export default App;
