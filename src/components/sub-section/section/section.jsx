eff1cadaf71cd2e41c18584817e15ae19de8992f
VaibhavDhanani <vaibhavdhanani2711@gmail.com>
2024-05-07 15:07:20 +0530

    some change

---
+++ src/components/sub-section/section/section.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from '../card/card'
+
+const Section = () => {
+    return (
+        <div className='block'>
+            <h1 className='text-left my-2 ml-5 text-2xl text-white font-semibold'>K-Dramas</h1>
+            <Card />
+        </div>
+    )
+}
+
+export default Section;
